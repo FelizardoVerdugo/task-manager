1a35966139cfe8bebc21cafc6d7d0853e528a897
Felizardo Verdugo <fm.verdugo20@info.uas.edu.mx>
2026-05-17 12:27:03 -0600

    se agregan las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'tasks/:id', component: TaskDetailComponent },
+];
